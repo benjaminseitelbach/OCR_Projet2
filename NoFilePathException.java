395bdbd24124ef21fe0bb8dc18e3ca65472603c1
Benjamin SEITELBACH <39366365+benjaminseitelbach@users.noreply.github.com>
2020-02-09 22:22:39 +0100

    Delete NoFilePathException.java

--- NoFilePathException.java
+++
@@ -1,8 +0,0 @@
-package com.hemebiotech.analytics;
-
-public class NoFilePathException extends Exception {
-	public NoFilePathException() {
-		System.out.println("Please set filepath in Config class");
-	}
-
-}
